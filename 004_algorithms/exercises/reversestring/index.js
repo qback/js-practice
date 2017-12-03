e5c15358b3dfb94287282f8ab28bd40a0c157023
Oleg <qbacknn@gmail.com>
2017-12-03 19:54:16 +0300

    add reversestring solutions

--- 004_algorithms/exercises/reversestring/index.js
+++ 004_algorithms/exercises/reversestring/index.js
@@ -6,6 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+	// return str.split('').reduce((previous, current) => current + previous, '');
+
+	// let reversed = '';
+	// for (const element of str) {
+	// 	reversed = element + reversed;
+	// }
+	// return reversed;
+
+	// let newStr = '';
+	// for (let index = str.length; index > 0; index--) {
+	// 	newStr += str[index - 1];
+	// }
+	// return newStr;
+
+	// return str.split('').reverse().join('');
+
+	return [...str].reverse().join('');
+}
 
 module.exports = reverse;
