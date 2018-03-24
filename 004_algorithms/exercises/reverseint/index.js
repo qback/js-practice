2314214adfd50bacd8b6414768bb106949cc0a86
Oleg <qbacknn@gmail.com>
2018-03-24 20:30:41 +0300

    add reverseint

--- 004_algorithms/exercises/reverseint/index.js
+++ 004_algorithms/exercises/reverseint/index.js
@@ -8,6 +8,13 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  /*  const reverseAbsStr = Math.abs(n).toString().split('').reverse().join('');
+   const reverseAbsInt = parseInt(reverseAbsStr, 10);
+   return Math.sign(n) !== -1 ? reverseAbsInt : reverseAbsInt * -1; */
+
+  const reverseStr = n.toString().split('').reverse().join('');
+  return parseInt(reverseStr, 10) * Math.sign(n);
+}
 
 module.exports = reverseInt;
